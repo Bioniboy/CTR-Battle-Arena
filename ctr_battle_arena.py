615697246f23384e514f14d6269de9c4fab31343
Bioniboy <bioniboystuff@zmagic.com>
2020-11-16 15:29:33 -0700

    Fixed movement

--- ctr_battle_arena.py
+++ ctr_battle_arena.py
@@ -107,7 +107,7 @@ class Player(Actor):
 
     def on_key_press(self, key, can_jump):
         if key in [arcade.key.UP, arcade.key.W, arcade.key.SPACE] and can_jump:
-            self.move(y_vel=JUMP_SPEED)
+            self.change_y = JUMP_SPEED
         elif key in [arcade.key.LEFT, arcade.key.A] and self.left > LEFT_LIMIT:
             self.change_x = -MOVEMENT_SPEED
             self.texture = self.textures[1]
@@ -118,7 +118,7 @@ class Player(Actor):
     def on_key_release(self, key):
         if (key in [arcade.key.LEFT, arcade.key.A] and self.change_x < 0
                 or key in [arcade.key.RIGHT, arcade.key.D] and self.change_x > 0):
-            self.move(x_vel=0)
+            self.change_x = 0
         if key in [arcade.key.UP, arcade.key.W, arcade.key.SPACE] and self.change_y > 0:
             self.change_y *= 0.5
     
